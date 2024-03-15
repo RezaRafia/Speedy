eb1daac1af20d68903f6f98bc3fbe3d25f8c6d66
reza rafia <rezarafia98@gmail.com>
2024-03-15 21:02:54 +0330

    a

--- forge.config.ts
+++ forge.config.ts
@@ -6,9 +6,7 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 
 const config: ForgeConfig = {
-  packagerConfig: {
-    asar: true,
-  },
+
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   publishers: [
