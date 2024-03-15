10969012b6b97bd9795643e0de38f177f473efb2
reza rafia <rezarafia98@gmail.com>
2024-03-16 01:30:28 +0330

    done

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ const createWindow = () => {
   }
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
